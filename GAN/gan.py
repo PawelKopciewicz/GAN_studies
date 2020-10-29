484b7f4fe2e4cd599c53747af8dc73a363eb7b17
Mordokrowka <48591214+Mordokrowka@users.noreply.github.com>
2020-10-29 12:13:16 +0100

    Update gan.py

--- GAN/gan.py
+++ GAN/gan.py
@@ -147,4 +147,3 @@ for epoch in range(opt.n_epochs):
         if batches_done % opt.sample_interval == 0:
             save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
 
-
