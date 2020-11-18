4a3ce572258c4e96b63933fe02df1dfd8d58d743
Mordokrowka <48591214+Mordokrowka@users.noreply.github.com>
2020-11-19 00:47:12 +0100

    Update gan_lightning.py
    Lightning working with a little loss printing issue

--- GAN_PyTorch/gan_lightning.py
+++ GAN_PyTorch/gan_lightning.py
@@ -22,6 +22,8 @@ os.makedirs("images", exist_ok=True)
 opt = options.options().opt
 print(opt)
 
+
+
 #img_shape = (opt.channels, opt.img_size, opt.img_size)
 Dataset = RootDataSet.RootDataSet("../files/PhaseSpaceSimulation.root")
 
@@ -48,34 +50,46 @@ class GAN(pl.LightningModule):
             nn.Linear(64, 1),
             nn.Sigmoid(),
         )
-    def forward(self, z, setup):
-        if setup == "Generator":
-            img = self.Generator(z)
-            return img
-        if setup == "Discriminator":
-            validity = self.model(z)  # (img_flat)
-            return validity
+
     def configure_optimizers(self):
         opt_gen = torch.optim.Adam(self.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
         opt_disc = torch.optim.Adam(self.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
         return (opt_gen, opt_disc)
-    def training_step(self, batch, batch_idx):
-        # training_step defined the train loop. It is independent of forward
-        x, y = batch
-        x = x.view(x.size(0), -1)
-        z = self.encoder(x)
-        x_hat = self.decoder(z)
-        loss = F.mse_loss(x_hat, x)
-        self.log('train_loss', loss)
-        return loss
+    
+    def training_step(self, batch, batch_idx, optimizer_idx):
+
+        (opt_gen, opt_disc) = self.optimizers()
+
+        #ground truths
+        valid = Variable(torch.FloatTensor(batch.size(0), 1).fill_(1.0), requires_grad=False)
+        fake = Variable(torch.FloatTensor(batch.size(0), 1).fill_(0.0), requires_grad=False)
+        real_imgs = Variable(batch.type(torch.FloatTensor))
+
+        opt_gen.zero_grad()
+        z = Variable(torch.FloatTensor(np.random.normal(0, 1, (batch.shape[0], opt.latent_dim))))
+
+        gen_imgs = self.Generator(z)
+
+        # Loss measures generator's ability to fool the discriminator
+        g_loss = torch.nn.BCELoss()(self.Discriminator(gen_imgs), valid)
+
+        print('\n',g_loss)
+        g_loss.backward()
+        opt_gen.step()
+
+        opt_disc.zero_grad()
+
+        # Measure discriminator's ability to classify real from generated samples
+        real_loss = torch.nn.BCELoss()(self.Discriminator(real_imgs), valid)
+
+        fake_loss = torch.nn.BCELoss()(self.Discriminator(gen_imgs.detach()), fake)
+        d_loss = (real_loss + fake_loss) / 2
+
+        d_loss.backward()
+        opt_disc.step()
 
 
-
-# Loss function
 adversarial_loss = torch.nn.BCELoss()
-
-#generator = Generator()
-#discriminator = Discriminator()
 
 GenAdvNet = GAN()
 
@@ -85,65 +99,6 @@ dataloader = torch.utils.data.DataLoader(
     shuffle=True,
 )
 
-# Optimizers
-optimizer_G = torch.optim.Adam(self.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-optimizer_D = torch.optim.Adam(GenAdvNet.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-
-Tensor = torch.FloatTensor
-
-# ----------
-#  Training
-# ----------
-
-for epoch in range(opt.n_epochs):
-    for i,imgs in enumerate(dataloader):
-        #print(imgs)
-        #print(q)
-
-        # Adversarial ground truths
-        valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
-        fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
-        #print(valid)
-        # Configure input
-        real_imgs = Variable(imgs.type(Tensor))
-        #print(real_imgs)
-
-        optimizer_G.zero_grad()
-
-        # Sample noise as generator input
-        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
-        #print(z)
-        #print(imgs.shape[0])
-        # Generate a batch of images
-        gen_imgs = GenAdvNet(z, "Generator")
-        #print(gen_imgs)
-        # Loss measures generator's ability to fool the discriminator
-        g_loss = adversarial_loss(GenAdvNet(gen_imgs, "Discriminator"), valid)
-
-        g_loss.backward()
-        optimizer_G.step()
-
-        # ---------------------
-        #  Train Discriminator
-        # ---------------------
-
-        optimizer_D.zero_grad()
-
-        # Measure discriminator's ability to classify real from generated samples
-        real_loss = adversarial_loss(GenAdvNet(real_imgs, "Discriminator"), valid)
-        fake_loss = adversarial_loss(GenAdvNet(gen_imgs.detach(),"Discriminator"), fake)
-        d_loss = (real_loss + fake_loss) / 2
-
-        d_loss.backward()
-        optimizer_D.step()
-
-        print(
-            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
-        )
-
-        batches_done = epoch * len(dataloader) + i
-        if batches_done % opt.sample_interval == 0:
-            save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
-
+gan = GAN()
+pl.Trainer().fit(gan, dataloader)
 
